2a010c47a6d94ef87af2f3b90565f83587f3917a
Ashnaali3255 <ashnaalipk@gmail.com>
2023-07-17 19:59:03 +0500

    Update item.rb file

---
+++ item.rb
@@ -0,0 +1,23 @@
+class Item
+  attr_accessor :name, :published_date, :archived
+
+  def initialize(name, published_date)
+    @name = name
+    @published_date = published_date
+    @archived = false
+  end
+
+  def can_be_archived?
+    today = Date.today
+    published_date < (today - 10 * 365) 
+  end
+
+  def move_to_archive
+    if can_be_archived?
+      @archived = true
+      puts "#{name} has been archived."
+    else
+      puts "#{name} cannot be archived."
+    end
+  end
+end
